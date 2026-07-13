f31386dbce782f52beed5e4e1467a32ba70b685b
angela-rojas05 <angela.rojas@tecdesoftware.edu.mx>
2026-07-13 08:03:47 -0600

    feat(purchase): implement full purchase flow architecture
    - Add Purchase and PurchaseItem domain models
    
    - Configure Compra and CompraProducto JPA entities with cascade rules
    
    - Implement MapStruct mappers (PurchaseMapper, PurchaseItemMapper)
    
    - Build CompraCrudRepository with custom query method
    
    - Add PurchaseService to delegate business logic
    
    - Expose GET and POST endpoints in PurchaseController
    
    - FINAL EDITION

--- src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/domain/service/PurchaseService.java
+++ src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/domain/service/PurchaseService.java
@@ -1,4 +1,28 @@
 package mx.edu.tecdesoftware.market_backend_2026_3_b.domain.service;
 
+import mx.edu.tecdesoftware.market_backend_2026_3_b.domain.Purchase;
+import mx.edu.tecdesoftware.market_backend_2026_3_b.domain.repository.PurchaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PurchaseService {
+
+    @Autowired
+    private PurchaseRepository purchaseRepository;
+
+    public List<Purchase> getAll() {
+        return purchaseRepository.getAll();
+    }
+
+    public Optional<List<Purchase>> getByClient(String clientId) {
+        return purchaseRepository.getByClient(clientId);
+    }
+
+    public Purchase save(Purchase purchase) {
+        return purchaseRepository.save(purchase);
+    }
 }
