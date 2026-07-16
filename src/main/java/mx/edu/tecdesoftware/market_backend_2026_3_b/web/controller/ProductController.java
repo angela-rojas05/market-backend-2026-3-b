464344203a929f528fd031224965a89ea306a3ed
angela-rojas05 <angela.rojas@tecdesoftware.edu.mx>
2026-07-16 11:32:27 -0600

    feat(market): implement and fix full product and purchase endpoints

--- src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/web/controller/ProductController.java
+++ src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/web/controller/ProductController.java
@@ -1,5 +1,11 @@
 package mx.edu.tecdesoftware.market_backend_2026_3_b.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import mx.edu.tecdesoftware.market_backend_2026_3_b.domain.Product;
 import mx.edu.tecdesoftware.market_backend_2026_3_b.domain.repository.ProductRepository;
 import mx.edu.tecdesoftware.market_backend_2026_3_b.domain.service.ProductService;
@@ -12,25 +18,52 @@ import java.util.List;
 
 @RestController
 @RequestMapping ("/products")
+@Tag(name = "Product", description = "Manage products in the store")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
     @GetMapping("")
+    @Operation(summary = "Get all products",
+            description = "Return a list of all available products")
+    @ApiResponse(responseCode = "200",
+    description = "Successful retrieval of products")
+    @ApiResponse(responseCode = "500",
+    description = "Internal server error")
     public ResponseEntity<List<Product>> gellAll(){
         return ResponseEntity.ok(productService.getAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId){
+    @Operation(summary = "Get product by ID",
+            description = "Return a product by its ID if it exists")
+    @ApiResponse(responseCode = "200",
+            description = "Product found")
+    @ApiResponse(responseCode = "404", description = "Product not found")
+    @ApiResponse(responseCode = "500",
+            description = "Internal server error")
+    public ResponseEntity<Product> getProduct(
+            @Parameter (description = "ID of the product to be retrieved",
+            example = "7", required = true)
+            @PathVariable("id") int productId){
         return productService.getProduct(productId)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping("/category/{categoryId}")
-    public ResponseEntity<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId) {
+    @Operation(summary = "Get products by category",
+            description = "Return all products in a specific category")
+    @ApiResponse(responseCode = "200",
+            description = "Product(s) found in the category")
+    @ApiResponse(responseCode = "404", description = "Product(s) not found in the category")
+    @ApiResponse(responseCode = "500",
+            description = "Internal server error")
+    public ResponseEntity<List<Product>> getByCategory(
+            @Parameter (description = "Category ID",
+                    example = "7", required = true)
+            int categoryId) {
         return productService.getByCategory(categoryId)
                 .filter(products -> !products.isEmpty())
                 .map(ResponseEntity::ok)
@@ -38,11 +71,45 @@ public class ProductController {
     }
 
     @PostMapping("")
+    @Operation(summary = "Create a new product",
+    description = "Register a new product and return it",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            required = true,
+            content = @Content(
+                    examples = @ExampleObject(
+                            name = "Example Porduct",
+                    value =
+                    """
+                    { 
+                        "name" : "Helado",
+                        "categoryId" : 5,
+                        "price" : "19.50",
+                        "stock" : 300,
+                        "active" : true  
+                    }
+                    """
+                    )
+                )
+            )
+    )
+    @ApiResponse(responseCode = "201", description = "Product created successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid product data")
+    @ApiResponse(responseCode = "401", description = "Unauthorized")
+    @ApiResponse(responseCode = "403", description = "Forbidden")
+    @ApiResponse(responseCode = "409", description = "Product conflict (duplicate code or SKU)")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public ResponseEntity<Product> save(@RequestBody Product product){
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(product));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a product by ID", description = "Deletes a product if it exists")
+    @ApiResponse(responseCode = "200", description = "Product deleted successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid product ID")
+    @ApiResponse(responseCode = "401", description = "Unauthorized")
+    @ApiResponse(responseCode = "403", description = "Forbidden")
+    @ApiResponse(responseCode = "404", description = "Product not found")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public ResponseEntity<Boolean> delete(@PathVariable("id") int productId){
         if (productService.delete(productId)) {
             return ResponseEntity.ok(true);
--- src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/web/controller/PurchaseController.java
+++ src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/web/controller/PurchaseController.java
@@ -1,5 +1,11 @@
 package mx.edu.tecdesoftware.market_backend_2026_3_b.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import mx.edu.tecdesoftware.market_backend_2026_3_b.domain.Purchase;
 import mx.edu.tecdesoftware.market_backend_2026_3_b.domain.service.PurchaseService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +17,19 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/purchases")
+@Tag(name = "Purchase", description = "Manage purchases in the store")
 public class PurchaseController {
 
     @Autowired
     private PurchaseService purchaseService;
 
     @GetMapping("/all")
+    @Operation(summary = "Get all purchases",
+            description = "Return a list of all available purchases")
+    @ApiResponse(responseCode = "200",
+            description = "Successful retrieval of purchases")
+    @ApiResponse(responseCode = "500",
+            description = "Internal server error")
     public ResponseEntity<List<Purchase>> getAll() {
         return new ResponseEntity<>(
                 purchaseService.getAll(),
@@ -25,7 +38,16 @@ public class PurchaseController {
     }
 
     @GetMapping("/client/{id}")
+    @Operation(summary = "Get purchases by clients",
+            description = "Return all purchases of a specific client")
+    @ApiResponse(responseCode = "200",
+            description = "Purchase(s) found in the category")
+    @ApiResponse(responseCode = "404", description = "Purchase(s) not found in the category")
+    @ApiResponse(responseCode = "500",
+            description = "Internal server error")
     public ResponseEntity<List<Purchase>> getByClient(
+            @Parameter(description = "Client ID",
+                    example = "4546221", required = true)
             @PathVariable("id") int clientId) {
 
         return purchaseService.getByClient(clientId)
@@ -39,6 +61,32 @@ public class PurchaseController {
     }
 
     @PostMapping("/save")
+    @Operation(summary = "Create a new purchase",
+            description = "Register a new purchase and return it",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    required = true,
+                    content = @Content(
+                            examples = @ExampleObject(
+                                    name = "Example Purchase",
+                                    value = """
+            { 
+                "clientId": "2552243",
+                "date": "2026-07-16T17:12:47.176Z",
+                "paymentMethod": "E",
+                "comment": "Prueba purchase",
+                "state": "P"
+            }
+            """
+                            )
+                    )
+            )
+    )
+    @ApiResponse(responseCode = "201", description = "Purchase created successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid purchase data")
+    @ApiResponse(responseCode = "401", description = "Unauthorized")
+    @ApiResponse(responseCode = "403", description = "Forbidden")
+    @ApiResponse(responseCode = "409", description = "Purchase conflict (duplicate)")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public ResponseEntity<Purchase> save(
             @RequestBody Purchase purchase) {
 
